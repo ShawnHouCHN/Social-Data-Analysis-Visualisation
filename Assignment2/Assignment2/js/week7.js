a5b466ec915dceee3561b042e89d5bd507bde98b
ShawnHouCHN <houxiaoshen81@gmail.com>
2017-03-22 20:49:11 +0100

    template for now

--- Assignment2/Assignment2/js/week7.js
+++ Assignment2/Assignment2/js/week7.js
@@ -1,8 +1,5 @@
 var dataDicts; 
-var disDicts,disTotalDicts;
-var disCleanDict = {};
-var disArray=[], pos03=[], pos15=[], theft03=[],theft15=[],totalcrime03=[],totalcrime15=[];
-var data03=[],data15=[];
+
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 1160 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -12,51 +9,31 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
 
 
 //load data
-dataDicts = d3.csv("data/SFPD_Incidents_-_from_1_January_2003.csv", function(data) {
-  //console.log(data);
-  disDicts=aggregateDistricts(data);
-  disTotalDicts=aggregateCrimePerDistrict(data);
+dataDicts = d3.json("data/crime_2003.json", function(error, data) {
+  alert(error);
+  console.log(data);
 
-  //12 is the index of 2015, 0 is 2013 in the array
-  theft03=disDicts[0].values[1];
-  theft15=disDicts[12].values[1];
-  pos03=disDicts[0].values[2];
-  pos15=disDicts[12].values[2];
-
-  totalcrime03=disTotalDicts[0];
-  totalcrime15=disTotalDicts[12];
-
-  for (index=0; index < totalcrime03.values.length; index++){
-  	data03.push({'POS':pos03.values[index].value,'THE':theft03.values[index].value,'TOTAL':totalcrime03.values[index].value, 'DIS': totalcrime03.values[index].key})
-  }
-
-  for (index=0; index < totalcrime15.values.length; index++){
-  	data15.push({'POS':pos15.values[index].value,'THE':theft15.values[index].value,'TOTAL':totalcrime15.values[index].value, 'DIS': totalcrime15.values[index].key})
-  }
-
-
-  	var yScale = d3.scaleLinear()
-			  .domain(d3.extent(theft03.values,function(d){return d.value;}))
+  var yScale = d3.scaleLinear()
+			  .domain(d3.extent(data,function(d){return d.VT;}))
 			  .range([height,0])
 			  .nice();
 
 	var yAxis = d3.axisLeft(yScale);
-//svg.call(yAxis);
 
 	var xScale = d3.scaleLinear()
-			   .domain(d3.extent(pos03.values,function(d){return d.value;}))
+			   .domain(d3.extent(data,function(d){return d.PR;}))
 			   .range([0,width])
 			   .nice();
 
 	var xAxis = d3.axisBottom(xScale).ticks(5);
 
 	var rScale= d3.scaleSqrt()
-				  .domain([0,d3.max(totalcrime03.values,function(d){return d.value;})])
+				  .domain([0,d3.max(data,function(d){return d.TOT;})])
 				  .range([0,20]);
 
 	// setup fill color
 	// setup fill color
-	var cValue = function(d) { return d.TOTAL;}, color = d3.scaleOrdinal(d3.schemeCategory10);
+	var cValue = function(d) { return d.TOT;}, color = d3.scaleOrdinal(d3.schemeCategory10);
 
 	// x-axis
 	svg.append("g")
@@ -71,7 +48,7 @@ dataDicts = d3.csv("data/SFPD_Incidents_-_from_1_January_2003.csv", function(dat
       .style("text-anchor", "end");
 
   	// y-axis
-  	svg.append("g")
+  svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
       .append("text")
@@ -85,17 +62,17 @@ dataDicts = d3.csv("data/SFPD_Incidents_-_from_1_January_2003.csv", function(dat
 
 
    	svg.selectAll('circle')
-   	   .data(data03)
+   	   .data(data)
    	   .enter()
    	   .append('circle')
    	   .attr('cx',function(d){
-   	   	return xScale(d.POS);
+   	   	return xScale(d.PR);
    	   })
    	   .attr('cy',function(d){
-   	   	return yScale(d.THE);
+   	   	return yScale(d.VT);
    	   })
    	   .attr('r',function(d){
-   	   	return rScale(d.TOTAL);
+   	   	return rScale(d.TOT);
    	   })
    	   .style('fill-opacity',0.7)
    	   .style('fill', function(d) { return color(cValue(d));})
@@ -103,8 +80,8 @@ dataDicts = d3.csv("data/SFPD_Incidents_-_from_1_January_2003.csv", function(dat
           tooltip.transition()
                .duration(200)
                .style("opacity", 0.8);
-          tooltip.html(d.DIS + "<br/> (" + d.POS
-	        + ", " + d.THE + ")")
+          tooltip.html(d.DST + "<br/> (" + d.PR
+	        + ", " + d.VT + ")")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
@@ -112,7 +89,7 @@ dataDicts = d3.csv("data/SFPD_Incidents_-_from_1_January_2003.csv", function(dat
           tooltip.transition()
                .duration(500)
                .style("opacity", 0);
-      });;
+      });
 
     
       // draw legend
@@ -161,41 +138,5 @@ var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-
-
-
-
-function aggregateDistricts(data){
-	disDicts=d3.nest().key(function(d) { 
- 		return d.Date.slice(-4);
- 	}).sortKeys(d3.ascending)
- 	.key(function(d) {
- 		if(d.Category=="PROSTITUTION" || d.Category=="VEHICLE THEFT"){
- 			return d.Category;
- 		}else{
- 			return "Others";
- 		}
- 	}).key(function(d) { 
- 		return d.PdDistrict;
- 	}).sortKeys(d3.ascending)
- 	.rollup(function(leaves) { 
- 		return leaves.length; })
- 	.entries(data);
-
- 	return disDicts;
-}
-
-function aggregateCrimePerDistrict(data){
-	dislengthArray=d3.nest().key(function(d) { 
- 		return d.Date.slice(-4);
- 	}).sortKeys(d3.ascending)
- 	.key(function(d) { 
- 		return d.PdDistrict;
- 	}).sortKeys(d3.ascending)
- 	.rollup(function(leaves) { 
- 		return leaves.length; })
- 	.entries(data);
- 	return dislengthArray;
-}
 
 
