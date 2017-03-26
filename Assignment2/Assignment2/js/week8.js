3923efa1fc1c348f61803e63d278305c81e7ebe9
ShawnHouCHN <houxiaoshen81@gmail.com>
2017-03-26 20:40:26 +0200

    ...

--- Assignment2/Assignment2/js/week8.js
+++ Assignment2/Assignment2/js/week8.js
@@ -23,168 +23,206 @@ var svg2 = d3.select("#maintwo")
 			.style("margin","auto");
 
 
-//Load in GeoJSON data
+// Initialize global data variables
+var KM_data;			// to hold PR crimes
+var json_data;    // to hold SF boundaries
+var center_data;	// to hold cluster centers
+var dist_data;		// to hold district names
 
+// cluster stabilities
+var stabs = [0.9918, 0.9927, 0.9075, 0.9154, 0.9014];
 
-d3.json("data/sfpddistricts.geojson", function(json) {
+d3.json("data/dist_locs.json", function(centers) {
 
-	//Bind data and create one path per GeoJSON feature
-	svg2.selectAll("path")
-	   .data(json.features)
-	   .enter()
-	   .append("path")
-		 // the location is a call to path, which takes the feature
-		 // (coordinates) that we pass to it, and makes it into an svg
-	   .attr("d", path)
-		.style("fill", "lightgray")
-		 .attr("stroke", "rgba(0, 0, 0, 0.5)")
-		 .attr("stroke-width", 0.5);
+	dist_data = centers;
 
-		 // plot the PR dots
-		 d3.json("data/kmeansPR-values.json", function(data) {
+}); // end dist names
 
-			 svg2.selectAll("circle")
-				 .data(data)
-				 .enter()
-				 .append("circle")
-				 .attr("cx", function(d) {
-								 return projection([d[0], d[1]])[0];
-				 })
-				 .attr("cy", function(d) {
-								 return projection([d[0], d[1]])[1];
-				 })
-				 .attr("r", 1.8)
-				 .style("fill", function(d) {
-					 return colors(d[2]);
-				 })
-				 .style("opacity", 0.75);
+// load centers
+d3.json("data/cluster_centers.json", function(centers) {
 
-				 // plot original centers for k2
-				 d3.json("data/cluster_centers.json", function(centers) {
+	center_data = centers;
 
-					 var curr_centers = centers[0];
+}); 
 
-					 svg2.selectAll(".center")
-					 	.data(curr_centers)
-						.enter()
-						.append("circle")
-						.attr("cx", function(d) {
-										return projection([d[0], d[1]])[0];
-						})
-						.attr("cy", function(d) {
-										return projection([d[0], d[1]])[1];
-						})
-						.attr("r", 2.8)
-						.attr("class", "center")
-						.style("fill", 'black');
+d3.json("data/sfpddistricts.geojson", function(data1) {
 
-				 }) // end orig centers, keep wrapped in dots so dot's don't overlap
+			// plot the PR dots
+			d3.json("data/kmeansPR-values.json", function(data2) {
 
-				 // plot neighbourhood names
-				 d3.json("data/dist_locs.json", function(centers) {
+				json_data = data1;
+				KM_data = data2;
 
-					 console.log(centers);
+			// bind data and create one path per GeoJSON feature
+				svg2.selectAll("path")
+			   .data(json_data.features)
+			   .enter()
+			   .append("path")
+				 // the location is a call to path, which takes the feature
+				 // (coordinates) that we pass to it, and makes it into an svg
+			   .attr("d", path)
+				 .style("fill", "lightgray")
+				 .attr("stroke", "rgba(0, 0, 0, 0.5)")
+				 .attr("stroke-width", 0.5);
 
-					 svg2.selectAll(".dist_name")
-						.data(centers)
-						.enter()
-						.append("text")
-						.attr("x", function(d) {
-										return projection(d.loc)[0];
-						})
-						.attr("y", function(d) {
-										return projection(d.loc)[1];
-						})
-						.attr("class", "dist_name")
-						.text(function(d) {
-							return d.dist;
-						})
+				 // plot district names (under dots)
+				 svg2.selectAll(".dist_name")
+					.data(dist_data)
+					.enter()
+					.append("text")
+					.attr("x", function(d) {
+									return projection(d.loc)[0];
+					})
+					.attr("y", function(d) {
+									return projection(d.loc)[1];
+					})
+					.attr("class", "dist_name")
+					.text(function(d) {
+						return d.dist;
+					});
 
-				 }) // end orig centers, keep wrapped in dots so dot's don't overlap
-
-	}) // end orig kmeans json
-
-
-
-		 // ## For interactivity ##
-d3.selectAll("button")
- .on("click", function() {
-
-				 // grab the element name
-				 var K_neigh = String(d3.select(this).text()).slice(-1);
-
-				 // plot points
-				 d3.json("data/kmeansPR-values.json", function(data) {
-
+					 // plot default PR crimes for k2 (over names)
 					 svg2.selectAll("circle")
-						 .data(data)
-						 .transition()
-						 .style("fill", function(d) {
-							 return colors(d[K_neigh]);
+						 .data(KM_data)
+						 .enter()
+						 .append("circle")
+						 .attr("cx", function(d) {
+										 return projection([d[0], d[1]])[0];
 						 })
+						 .attr("cy", function(d) {
+										 return projection([d[0], d[1]])[1];
+						 })
+						 .attr("r", 1.8)
+						 .style("fill", function(d) {
+							 return colors(d[2]);
+						 })
+						 .style("opacity", 0.75);
+
+						 // plot cluster centers (over dots)
+						 var curr_centers = center_data[0];
+						 svg2.selectAll(".center")
+							.data(curr_centers)
+							.enter()
+							.append("circle")
+							.attr("cx", function(d) {
+											return projection([d[0], d[1]])[0];
+							})
+							.attr("cy", function(d) {
+											return projection([d[0], d[1]])[1];
+							})
+							.attr("r", 5.0)
+							.attr("class", "center")
+							// plot based on cluster
+							.style("fill", function(d, i) {
+								return "white";
+							})
+							.style("stroke", "black");
+
+						 // add legend
+						 var legend = svg2.selectAll(".legend")
+								.data([1])
+								.enter().append("g")
+								.attr("class", "legend")
+								.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+						// add cluster name
+						legend.append("text")
+								.attr("x", 0)
+								.attr("y", 9)
+								.attr("dy", ".35em")
+								.attr("font-size", "14px")
+								.attr("class", "k_num")
+								.style("text-anchor", "start")
+								.text(function (d) {
+									return 'Showing K:2';
+								});
+
+						// add cluster stability
+						legend.append("text")
+								.attr("x", 0)
+								.attr("y", 27)
+								.attr("dy", ".35em")
+								.attr("font-size", "14px")
+								.attr("class", "k_stability")
+								.style("text-anchor", "start")
+								.text(function (d) {
+									return 'Clustering stability: ' + String(stabs[0]);
+								});
+						});
+		});
 
 
-						 // dynamic plot the centers on click
-						 d3.json("cluster_centers.json", function(centers) {
+d3.selectAll(".tabk").on("click", function() {
 
-							 curr_centers = centers[K_neigh - 2];
+	 // grab the element name
+	 var K_neigh = String(d3.select(this).text()).slice(-1);
+	  // plot points
+	 svg2
+	 svg2.selectAll("circle")
+			 //.data(KM_data)
+			 .transition()
+			 .style("fill", function(d) {
+				 return colors(d[K_neigh]);
+				 });
 
-							 svg2.selectAll(".center")
-							 	.remove();
+	  // dynamic plot the centers on click
+		var curr_centers = center_data[K_neigh - 2];
 
-							 svg2.selectAll(".center")
-								.data(curr_centers)
-								.enter()
-								.append("circle")
-								.attr("cx", function(d) {
-												return projection([d[0], d[1]])[0];
-								})
-								.attr("cy", function(d) {
-												return projection([d[0], d[1]])[1];
-								})
-								.attr("r", 2.8)
-								.attr("class", "center")
-								.style("fill", 'black');
+		 // remove previous centers
+		 svg2.selectAll(".center")
+		 	.remove();
 
+		 // plot new centres
+		 svg2.selectAll(".center")
+			.data(curr_centers)
+			.enter()
+			.append("circle")
+			.attr("cx", function(d) {
+							return projection([d[0], d[1]])[0];
+			})
+			.attr("cy", function(d) {
+							return projection([d[0], d[1]])[1];
+			})
+			.attr("r", 5.0)
+			.attr("class", "center")
+			// plot based on cluster
+			.style("fill", function(d, i) {
+				return "white";
+			})
+			.style("stroke", "black");
 
-						 }) // end click cluster centers load
-
-				 }) // end click kmeans json load
-
- }) // end on click
-
-			 .on("mouseover", function() {
-
-				 // grab the element name
-				var K_neigh = String(d3.select(this).text()).slice(-1);
-				console.log(K_neigh);
-
-				 // dynamic plot the centers on click
-				 d3.json("data/cluster_centers.json", function(centers) {
-
-					 curr_centers = centers[K_neigh - 2];
-					 console.log(curr_centers);
-
-					 svg2.selectAll(".hov_center")
-						.data(curr_centers)
-						.enter()
-						.append("circle")
-						.attr("cx", function(d) {
-										return projection([d[0], d[1]])[0];
-						})
-						.attr("cy", function(d) {
-										return projection([d[0], d[1]])[1];
-						})
-						.attr("r", 2.8)
-						.attr("class", "hov_center")
-						.style("fill", 'red');
+			// update number of K in legend
+			svg2.selectAll(".legend").remove();
 
 
-				 }) // end click cluster centers load
+			var legend = svg2.selectAll(".legend")
+					.data([1])
+					.enter().append("g")
+					.attr("class", "legend")
+					.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-			 });
+			// add cluster name
+			legend.append("text")
+					.attr("x", 0)
+					.attr("y", 9)
+					.attr("dy", ".35em")
+					.attr("font-size", "14px")
+					.attr("class", "k_num")
+					.style("text-anchor", "start")
+					.text(function (d) {
+						return 'Showing K:' + K_neigh;
+					});
 
-}); // end spfd.geojson load
+			// add cluster stability
+			legend.append("text")
+					.attr("x", 0)
+					.attr("y", 27)
+					.attr("dy", ".35em")
+					.attr("font-size", "14px")
+					.attr("class", "k_stability")
+					.style("text-anchor", "start")
+					.text(function (d) {
+						return 'Clustering stability: ' + String(stabs[K_neigh-2])
+					});
 
-// add district names
-// add
+}); 
