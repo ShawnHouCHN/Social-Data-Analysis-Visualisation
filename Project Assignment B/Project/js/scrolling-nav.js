567cfe1700ffad1e402cd53f9d4a10d18f10c48b
ShawnHouCHN <houxiaoshen81@gmail.com>
2017-04-25 12:30:52 +0200

    website updates

--- Project Assignment B/Project/js/scrolling-nav.js
+++ Project Assignment B/Project/js/scrolling-nav.js
@@ -13,16 +13,17 @@ $(document).ready(function(){
         // ===== Scroll to Top ==== 
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
-            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+            $('#return-to-top').fadeIn(500);    // Fade in the arrow
         } else {
-            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+            $('#return-to-top').fadeOut(500);   // Else fade out the arrow
         }
     });
     $('#return-to-top').click(function() {      // When arrow is clicked
-        $('body,html').animate({
+        $('html, body').stop().animate({
             scrollTop : 0                       // Scroll to top of body
-        }, 1500);
+        }, 1500, 'easeInOutExpo');
     });
+
 
 });
 
